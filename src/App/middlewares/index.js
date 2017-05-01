a8d9dad883d85d5b91c538b8be7d04c0b26b6c81
38elements <mh19820223@gmail.com>
2017-05-01 20:02:55 +0900

    Refactor LOCATION_CHANGE

--- src/App/middlewares/index.js
+++ src/App/middlewares/index.js
@@ -1,10 +1,11 @@
 import { matchRoutes } from 'react-router-config';
+import { LOCATION_CHANGE } from 'react-router-redux/reducer';
 import routes from '../routes'
 
 const logMiddleware = store => next => action => {
     // console.log('log before');
     // console.dir(action);
-    if (action.type ===  '@@router/LOCATION_CHANGE') {
+    if (action.type ===  LOCATION_CHANGE) {
         let route = matchRoutes(routes, action.payload.pathname);
         console.dir(route);
     }
